390e2bd9fb32a4c423f4e85c6539d70582f8ccbd
pavitar <41386653+pavitarsaini@users.noreply.github.com>
2020-10-05 21:29:22 -0400

    Updates

---
+++ precache-manifest.8845a44ef6ec9df4ec5e3a2d4868cf15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f31b1d4a9971c9b920d2cf1a6424405",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "66ac3098ea2febe19a14",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "042e0d2588b4d0f399bd",
+    "url": "/react-gh-pages/static/js/2.98142b22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.98142b22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66ac3098ea2febe19a14",
+    "url": "/react-gh-pages/static/js/main.e7950ccf.chunk.js"
+  },
+  {
+    "revision": "6f550f76ad7f8b7e5b47",
+    "url": "/react-gh-pages/static/js/runtime-main.5d13d03f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
