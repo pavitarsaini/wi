82329498ec115bcd41741609760a3ca06fa2c0f1
pavitar <41386653+pavitarsaini@users.noreply.github.com>
2020-10-13 08:33:26 -0400

    Updates

---
+++ precache-manifest.25614fce5b6b87ed0759bf067739e0e3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa5f08ee2e09be0adf2188759b7cc304",
+    "url": "/index.html"
+  },
+  {
+    "revision": "414293dd016aadf7a533",
+    "url": "/static/js/2.246a19ae.chunk.js"
+  },
+  {
+    "revision": "4f4c435c0a1826e99a7b",
+    "url": "/static/js/main.aad6acda.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
